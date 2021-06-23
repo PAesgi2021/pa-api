5178ee74784850913fea2df4e7a785bf9a3fb1db
ahmedine <krapulxx@gmail.com>
2021-06-23 19:30:25 +0200

    Fix + merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,9 @@ import {Module} from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {TodolistModule} from './api/ergon/todolist/todolist.module';
 import {TestController} from './api/ergon/test/test.controller';
-import {Task} from "./api/ergon/tasks/task.entity";
-import {User} from "./auth/user.entity";
-import { YtPostModule } from './api/yourturn/yt-post/yt-post.module';
-import { YtPost } from './api/yourturn/yt-post/entities/yt-post.entity';
-import { MessageModule } from './api/yourturn/message/message.module';
+import {YtPostModule} from './api/yourturn/yt-post/yt-post.module';
+import {YtPost} from './api/yourturn/yt-post/entities/yt-post.entity';
+import { Message } from './api/yourturn/message/entities/message.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -15,22 +13,12 @@ import { MessageModule } from './api/yourturn/message/message.module';
     port: 5432,
     username: 'postgres',
     password: 'root',
-    database: 'pa-java',
-    entities: [Task, User],
-    synchronize: true,
-  }),
-    TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'root',
     database: 'pa-angular',
-    entities: [YtPost],
+    entities: [YtPost, Message],
     synchronize: true,
   }),
-    TasksModule, AuthModule, TodolistModule, YtPostModule, MessageModule],
+
+    TodolistModule, YtPostModule],
   controllers: [TestController],
 })
-
 export class AppModule {}
