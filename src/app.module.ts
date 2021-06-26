5af7a05de36faed8287ff593a8e9c09fb7a33555
ahmedine <krapulxx@gmail.com>
2021-06-26 14:29:05 +0200

    fixed merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TodolistModule } from './api/ergon/todolist/todolist.module';
 import { TestController } from './api/ergon/test/test.controller';
 import { YtPostModule } from './api/yourturn/yt-post/yt-post.module';
 import { YtPost } from './api/yourturn/yt-post/entities/yt-post.entity';
 import { YtUserModule } from './api/yourturn/yt-user/yt-user.module';
 import {YtUser} from "./api/yourturn/yt-user/entities/yt-user.entity";
+import { MessageModule } from './api/yourturn/message/message.module';
+import { Message } from './api/yourturn/message/entities/message.entity';
 
 
 @Module({
@@ -16,15 +17,12 @@ import {YtUser} from "./api/yourturn/yt-user/entities/yt-user.entity";
     username: 'postgres',
     password: 'root',
     database: 'pa-angular',
-    entities: [YtPost, YtUser],
-    entities: [YtPost, Message],
+    entities: [YtPost, YtUser, Message],
     synchronize: true,
   }),
 
-    YtPostModule, YtUserModule],
+    YtPostModule, YtUserModule, MessageModule],
   controllers: [TestController],
-    YtPostModule, MessageModule],
-  controllers: [],
 })
 export class AppModule {
 }
