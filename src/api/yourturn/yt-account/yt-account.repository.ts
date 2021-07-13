a4921597b5a0a844fd35fa11dc6d76b1a72133e8
Mickael <akninemickael@gmail.com>
2021-07-13 19:03:28 +0200

    ajout du login

--- src/api/yourturn/yt-account/yt-account.repository.ts
+++ src/api/yourturn/yt-account/yt-account.repository.ts
@@ -1,26 +1,43 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { YtAccount } from './entities/yt-account.entity';
 import { InternalServerErrorException, Logger } from '@nestjs/common';
-import { YtCreateAccountDto } from './dto/yt-create-account.dto';
+import { YtAccountDto } from './dto/yt-account.dto';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(YtAccount)
 export class YtAccountRepository extends Repository<YtAccount> {
   private logger = new Logger('accountRepository');
 
-  async createAccount(createAccountDto: YtCreateAccountDto): Promise<YtAccount> {
+  async createAccount(createAccountDto: YtAccountDto): Promise<YtAccount> {
+    const {username, password} = createAccountDto;
 
     const account = new YtAccount();
-    account.username = createAccountDto.username;
-    account.password = createAccountDto.password;
+    account.username = username;
+    account.salt = await bcrypt.genSalt();
+    account.password = await this.hashPassword(password, account.salt);
 
     try {
       await account.save();
 
     } catch (error) {
-      this.logger.error('Failed to save this yt-message');
+      this.logger.error('Failed to register account');
       throw new InternalServerErrorException('Internal Server Error!');
     }
     return account;
+  }
+
+  async validateUserPassword(signInAccountDTO: YtAccountDto): Promise<{ id: number, username: string, password: string }> {
+    const {username, password} = signInAccountDTO;
+    const account = await this.findOne({username});
+    if (account && await account.validatePassword(password)) {
+      return account;
+    } else {
+      return null;
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 
 }
--- src/api/yourturn/yt-account/yt-account.service.ts
+++ src/api/yourturn/yt-account/yt-account.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
-import { YtCreateAccountDto } from './dto/yt-create-account.dto';
+import {Injectable, Logger, UnauthorizedException} from '@nestjs/common';
+import { YtAccountDto } from './dto/yt-account.dto';
 import { YtUpdateAccountDto } from './dto/yt-update-account.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { YtAccountRepository } from './yt-account.repository';
@@ -7,13 +7,25 @@ import { YtAccountRepository } from './yt-account.repository';
 
 @Injectable()
 export class YtAccountService {
-
+  private logger = new Logger('YtAccountService');
   constructor(
     @InjectRepository(YtAccountRepository) private accountRepository: YtAccountRepository) {
   }
 
-  create(createAccountDto: YtCreateAccountDto) {
+  create(createAccountDto: YtAccountDto) {
     return this.accountRepository.createAccount(createAccountDto);
+  }
+
+  async signIn(signInAccountDto: YtAccountDto): Promise<YtAccountDto> {
+    const account = await this.accountRepository.validateUserPassword(signInAccountDto);
+
+    if (!account) {
+      throw new UnauthorizedException('Invalid Credentials');
+    }
+
+    const response: YtAccountDto = {...account}
+    this.logger.debug(`Successfully Authenticated User ${JSON.stringify(response.username)}`);
+    return account;
   }
 
   findAll() {
